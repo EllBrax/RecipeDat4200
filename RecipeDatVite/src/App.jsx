880c96b63a3dc80b01e8f97ad89164bb8f8451bd
Stanley Williams <stanley.williams-3@selu.edu>
2025-10-15 12:22:35 -0500

    Home page and The Kitchen page basic outline.

--- RecipeDatVite/src/App.jsx
+++ RecipeDatVite/src/App.jsx
@@ -2,7 +2,8 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Cookbook from './pages/Cookbook'
-import NewRecipe from './pages/NewRecipe'
+import TheKitchen from "./pages/TheKitchen";
+
 
 export default function App() {
   return (
@@ -11,14 +12,14 @@ export default function App() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/cookbook">Cookbook</Link>
-        <Link to="/newrecipe">New Recipe</Link>
+        <Link to="/thekitchen">The Kitchen</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/cookbook" element={<Cookbook />}/>
-        <Route path="/newrecipe" element={<NewRecipe/>}/>
+        <Route path="/thekitchen" element={<TheKitchen/>}/>
       </Routes>
     </div>
   )
--- RecipeDatVite/src/components/Header.jsx
+++ RecipeDatVite/src/components/Header.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import "./index.css";
 
 const Header = () => {
   return (
     <header>
-      <nav className="navbar">
-        <h1>RecipeDat</h1>
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
+      <div className="container">          
+        <nav className="navbar">
+          <h1>RecipeDat</h1>
+          <ul className="nav-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/cookbook">Cookbook</Link></li>
+            <li><Link to="/thekitchen">The Kitchen</Link></li>
+            <li><Link to="/profile/me">Profile</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
